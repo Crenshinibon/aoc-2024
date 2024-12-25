2e1e6802eab7ebbb56c93e6951b8f268f56747dc
Dirk Porsche <dirk@accoma.de>
2024-12-25 23:44:04 +0100

    progress

---
+++ day20/partone.zig
@@ -0,0 +1,143 @@
+const std = @import("std");
+const input = @embedFile("./input.txt");
+
+const DIM = 141;
+
+pub fn printMat(m: *[DIM][DIM]u8) void {
+    for (0..DIM) |x| {
+        for (0..DIM) |y| {
+            std.debug.print("{c}", .{m[x][y]});
+        }
+        std.debug.print("\n", .{});
+    }
+}
+
+pub fn printPath(p: *std.StringHashMap(Node)) void {
+    var ite = p.iterator();
+    var counter: usize = 0;
+    while (ite.next()) |pair| {
+        std.debug.print("{} {s} {any}\n", .{ counter, pair.key_ptr.*, pair.value_ptr.* });
+        counter += 1;
+    }
+}
+
+const Cheat = struct {
+    target: []const u8,
+};
+
+const Pos = struct {
+    r: usize,
+    c: usize,
+};
+
+const Node = struct {
+    pos: Pos,
+    dist: usize,
+    cheats: ?[]Cheat,
+};
+
+pub fn key(pos: Pos, allocator: std.mem.Allocator) ![]const u8 {
+    const buf: []u8 = try allocator.alloc(u8, 7);
+    return try std.fmt.bufPrint(buf, "{}-{}", .{ pos.r, pos.c });
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    std.debug.print("Starting", .{});
+
+    var mat: [DIM][DIM]u8 = undefined;
+    var row: usize = 0;
+    var col: usize = 0;
+
+    var path = std.StringHashMap(Node).init(allocator);
+    defer path.deinit();
+
+    var start = Pos{
+        .r = 0,
+        .c = 0,
+    };
+    for (input) |c| {
+        if (c == '\n') {
+            continue;
+        }
+        if (c == 'S') {
+            start.r = row;
+            start.c = col;
+        }
+
+        mat[col][row] = c;
+        if (col == (DIM - 1)) {
+            row += 1;
+            col = 0;
+        } else {
+            col += 1;
+        }
+    }
+    printMat(&mat);
+
+    //build path
+
+    var dist: usize = 0;
+    var current = start;
+    while (true) {
+        const k = try key(current, allocator);
+        var currentNode = Node{
+            .pos = current,
+            .dist = dist,
+            .cheats = null,
+        };
+
+        mat[current.r][current.c] = 'O';
+        // go all the way up up
+        if (mat[current.r][current.c - 1] == '.') {
+            var cheatDist: usize = 2;
+            var cheatList = std.ArrayList(Cheat).init(allocator);
+            while (cheatDist < current.c) {
+                const cCol = current.c - cheatDist;
+                const cRow = current.r;
+                if (mat[cRow][cCol] == '.') {
+                    //cheat found
+                    const cheatTarget = try key(Pos{ .c = cCol, .r = cRow }, allocator);
+                    try cheatList.append(Cheat{ .target = cheatTarget });
+                }
+                cheatDist += 1;
+            }
+            currentNode.cheats = try cheatList.toOwnedSlice();
+
+            //std.debug.print("Going up\n", .{});
+            current = Pos{ .c = current.c - 1, .r = current.r };
+        } else if (mat[current.r][current.c + 1] == '.') {
+            //std.debug.print("Going down\n", .{});
+            current = Pos{ .c = current.c + 1, .r = current.r };
+        } else if (mat[current.r - 1][current.c] == '.') {
+            //std.debug.print("Going left\n", .{});
+            current = Pos{ .c = current.c, .r = current.r - 1 };
+        } else if (mat[current.r + 1][current.c] == '.') {
+            //std.debug.print("Going right\n", .{});
+            current = Pos{ .c = current.c, .r = current.r + 1 };
+        } else {
+            std.debug.print("End reached {} {}\n", .{ current.r, current.c });
+            break;
+            // must be the end
+        }
+
+        try path.put(k, currentNode);
+        dist += 1;
+    }
+
+    printMat(&mat);
+    printPath(&path);
+    //
+    //const buf: []u8 = try allocator.alloc(u8, 7);
+    //        errdefer allocator.free(buf);
+    //        const key = try std.fmt.bufPrint(buf, "{}-{}", .{col, row});
+    //        path.put(key, Node{
+    //            .c = col,
+    //            .r = row,
+    //            .
+    //        });
+    //
+    //
+}
