94af6b54b7958b2b03b75cf0575d664846485fc7
Dirk Porsche <dirkporsche78@googlemail.com>
2024-12-30 20:28:16 +0100

    9-2 still

--- day09/parttwo.zig
+++ day09/parttwo.zig
@@ -9,6 +9,81 @@ const Slot = struct {
     size: usize,
 };
 
+pub fn printAll(slots1: *std.ArrayList(Slot), slots2: *std.ArrayList(Slot), slots3: *std.ArrayList(Slot), slots4: *std.ArrayList(Slot)) void {
+    var counter: usize = 0;
+    const last_slot: Slot = slots1.items[slots1.items.len - 1];
+    const last_idx: usize = last_slot.start_idx + last_slot.size;
+    while (true) {
+        std.debug.print("{}: |", .{counter});
+        var found_1 = false;
+        for (slots1.items) |slot| {
+            const l_idx = slot.start_idx + slot.size - 1;
+            const f_idx = slot.start_idx;
+            const inside = counter >= f_idx and counter <= l_idx;
+            if (inside) {
+                found_1 = true;
+                std.debug.print("0", .{});
+            }
+        }
+        if (!found_1) {
+            std.debug.print("_", .{});
+        }
+        std.debug.print("|", .{});
+
+        var found_2 = false;
+        for (slots2.items) |slot| {
+            const l_idx = slot.start_idx + slot.size - 1;
+            const f_idx = slot.start_idx;
+            const inside = counter >= f_idx and counter <= l_idx;
+            if (inside) {
+                found_2 = true;
+                std.debug.print("F", .{});
+            }
+        }
+        if (!found_2) {
+            std.debug.print("_", .{});
+        }
+        std.debug.print("|", .{});
+
+        var found_3 = false;
+        for (slots3.items) |slot| {
+            const l_idx = slot.start_idx + slot.size - 1;
+            const f_idx = slot.start_idx;
+            const inside = counter >= f_idx and counter <= l_idx;
+            if (inside) {
+                found_3 = true;
+                std.debug.print("{}", .{slot.id});
+            }
+        }
+        if (!found_3) {
+            std.debug.print("_", .{});
+        }
+        std.debug.print("|", .{});
+
+        var found_4 = false;
+        for (slots4.items) |slot| {
+            const l_idx = slot.start_idx + slot.size - 1;
+            const f_idx = slot.start_idx;
+            const inside = counter >= f_idx and counter <= l_idx;
+            if (inside) {
+                found_4 = true;
+                std.debug.print("{}", .{slot.id});
+            }
+        }
+        if (!found_4) {
+            std.debug.print("_", .{});
+        }
+
+        std.debug.print("|\n", .{});
+
+        if (counter == last_idx) {
+            break;
+        }
+        counter += 1;
+    }
+    std.debug.print("\n", .{});
+}
+
 pub fn print(slots: *std.ArrayList(Slot)) void {
     var counter: usize = 0;
     const last_slot: Slot = slots.items[slots.items.len - 1];
@@ -88,62 +163,52 @@ pub fn main() !void {
             compressed_pos += 1;
         }
     }
-
     std.debug.print("max file idx: {}\n", .{start_idx - 1});
-    std.debug.print("files: {any}\n", .{files.items});
-    std.debug.print("empty: {any}\n", .{empty.items});
 
     var filled = std.ArrayList(Slot).init(allocator);
+    var copied = try files.clone();
 
-    var forward_idx: usize = 0;
-    try filled.append(files.items[forward_idx]);
-
-    for (0..(empty.items.len - 1)) |free_idx| {
+    var free_idx: usize = 0;
+    var cont = true;
+    
+    try filled.append(files.items[free_idx]);
+    //restart on every move ...
+    while(cont) {
         var free = empty.items[free_idx];
-
         var file_idx = files.items.len - 1;
+
         while (true) {
             var cf = files.items[file_idx];
-            if (cf.size < free.size) {
+            if (cf.size <= free.size) {
                 cf.start_idx = free.start_idx;
                 try filled.append(cf);
 
                 free.size = free.size - cf.size;
                 free.start_idx = free.start_idx + cf.size;
 
+                _ = files.orderedRemove(file_idx);
+                file_idx = files.items.len - 1;
                 continue;
             }
 
             if (free.size == 0) {
+                file_idx = files.items.len - 1;
                 break;
             }
 
             if (file_idx > 0) {
                 file_idx -= 1;
             } else {
+                file_idx = files.items.len - 1;
                 break;
             }
         }
-        forward_idx += 1;
-        try filled.append(files.items[forward_idx]);
-    }
-    std.debug.print("filled:\n", .{});
-    print(&filled);
-    const result: usize = 0;
 
-    //for (locations.items, 0..) |loc, idx| {
-    //    switch (loc) {
-    //        PosTag.file => {
-    //            result += loc.file.id * idx;
-    //            //std.debug.print("mult id:{} * index:{} = {} => total result: {}\n", .{ loc.file.id, idx, loc.file.id * idx, result });
-    //        },
-    //        PosTag.free => {
-    //            std.debug.print("{any} at {}\n", .{ loc, idx });
-    //            std.debug.print("{any} at {}\n", .{ locations.items[idx + 1], idx + 1 });
-    //            break;
-    //        },
-    //    }
-    //}
+        free_idx += 1;
+    }
+
+    printAll(&empty, &copied, &filled, &files);
+    const result: usize = 0;
 
     std.debug.print("Result: {}\n", .{result});
 }
