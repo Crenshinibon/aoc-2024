773db5ca03241b787b5729faa502c0c335328c57
Dirk Porsche <dirk@accoma.de>
2025-01-01 12:57:09 +0100

    nice solution for 09-2 but too good

--- day09/parttwo.zig
+++ day09/parttwo.zig
@@ -5,116 +5,16 @@ const fileName = "small.txt";
 const Slot = struct {
     id: usize,
     type: u8,
-    start_idx: usize,
     size: usize,
 };
 
-pub fn printAll(slots1: *std.ArrayList(Slot), slots2: *std.ArrayList(Slot), slots3: *std.ArrayList(Slot), slots4: *std.ArrayList(Slot)) void {
-    var counter: usize = 0;
-    const last_slot: Slot = slots1.items[slots1.items.len - 1];
-    const last_idx: usize = last_slot.start_idx + last_slot.size;
-    while (true) {
-        std.debug.print("{}: |", .{counter});
-        var found_1 = false;
-        for (slots1.items) |slot| {
-            const l_idx = slot.start_idx + slot.size - 1;
-            const f_idx = slot.start_idx;
-            const inside = counter >= f_idx and counter <= l_idx;
-            if (inside) {
-                found_1 = true;
-                std.debug.print("0", .{});
-            }
-        }
-        if (!found_1) {
-            std.debug.print("_", .{});
-        }
-        std.debug.print("|", .{});
-
-        var found_2 = false;
-        for (slots2.items) |slot| {
-            const l_idx = slot.start_idx + slot.size - 1;
-            const f_idx = slot.start_idx;
-            const inside = counter >= f_idx and counter <= l_idx;
-            if (inside) {
-                found_2 = true;
-                std.debug.print("F", .{});
-            }
-        }
-        if (!found_2) {
-            std.debug.print("_", .{});
-        }
-        std.debug.print("|", .{});
-
-        var found_3 = false;
-        for (slots3.items) |slot| {
-            const l_idx = slot.start_idx + slot.size - 1;
-            const f_idx = slot.start_idx;
-            const inside = counter >= f_idx and counter <= l_idx;
-            if (inside) {
-                found_3 = true;
-                std.debug.print("{}", .{slot.id});
-            }
-        }
-        if (!found_3) {
-            std.debug.print("_", .{});
-        }
-        std.debug.print("|", .{});
-
-        var found_4 = false;
-        for (slots4.items) |slot| {
-            const l_idx = slot.start_idx + slot.size - 1;
-            const f_idx = slot.start_idx;
-            const inside = counter >= f_idx and counter <= l_idx;
-            if (inside) {
-                found_4 = true;
-                std.debug.print("{}", .{slot.id});
-            }
-        }
-        if (!found_4) {
-            std.debug.print("_", .{});
-        }
-
-        std.debug.print("|\n", .{});
-
-        if (counter == last_idx) {
-            break;
-        }
-        counter += 1;
-    }
-    std.debug.print("\n", .{});
-}
-
 pub fn print(slots: *std.ArrayList(Slot)) void {
-    var counter: usize = 0;
-    const last_slot: Slot = slots.items[slots.items.len - 1];
-    const last_idx: usize = last_slot.start_idx + last_slot.size;
-    while (true) {
-        std.debug.print("{}: ", .{counter});
-        var found_one = false;
-        for (slots.items) |slot| {
-            const l_idx = slot.start_idx + slot.size - 1;
-            const f_idx = slot.start_idx;
-            const inside = counter >= f_idx and counter <= l_idx;
-            if (inside) {
-                found_one = true;
-                if (slot.type == 'F') {
-                    std.debug.print("{}", .{slot.id});
-                }
-                if (slot.type == 'E') {
-                    std.debug.print(".", .{});
-                }
-            }
+    for (slots.items, 0..) |slot, idx| {
+        if (slot.type == 'F') {
+            std.debug.print("{}: {c} => id: {} size: {}\n", .{ idx, slot.type, slot.id, slot.size });
+        } else {
+            std.debug.print("{}: {c} => emtpy size: {}\n", .{ idx, slot.type, slot.size });
         }
-        if (!found_one) {
-            std.debug.print("-", .{});
-        }
-
-        if (counter == last_idx) {
-            break;
-        }
-        counter += 1;
-
-        std.debug.print("\n", .{});
     }
     std.debug.print("\n", .{});
 }
@@ -127,10 +27,10 @@ pub fn findReverseFileIdx(slots: *std.ArrayList(Slot), target_len: usize) ?usize
             rev_index -= 1;
             continue;
         } else {
-            rev_index -= 1;
             if (rev_slot.size <= target_len) {
                 return rev_index;
             }
+            rev_index -= 1;
         }
     }
     return null;
@@ -149,36 +49,35 @@ pub fn main() !void {
     var slots = std.ArrayList(Slot).init(allocator);
 
     var compressed_pos: usize = 0;
-    var start_idx: usize = 0;
     while (true) {
         const byte = reader.readByte() catch |err| switch (err) {
             error.EndOfStream => break,
             else => return err,
         };
 
-        if (byte >= 48) {
+        if (byte >= 48 and byte <= 58) {
             const size: u8 = byte - 48;
             if (compressed_pos % 2 == 0) {
                 try slots.append(Slot{
                     .id = compressed_pos,
                     .type = 'F',
-                    .start_idx = start_idx,
                     .size = size,
                 });
             } else {
                 try slots.append(Slot{
                     .id = compressed_pos,
                     .type = 'E',
-                    .start_idx = start_idx,
                     .size = size,
                 });
             }
 
-            start_idx += size;
             compressed_pos += 1;
+        } else {
+            std.debug.print("Found other char: {}\n", .{byte});
         }
     }
-    std.debug.print("max file idx: {}\n", .{start_idx - 1});
+
+    print(&slots);
 
     var c_index: usize = 0;
     while (c_index < slots.items.len) {
@@ -197,21 +96,35 @@ pub fn main() !void {
                 std.debug.print("rev_index: {?}\n", .{rev_index});
 
                 // nothing found, continue
-                if (rev_index == null) {
+                if (rev_index == null or rev_index.? < c_index) {
                     break;
                 }
 
                 const rev_slot = slots.items[rev_index.?];
-                std.debug.print("rev_slot: {any}", .{rev_slot});
                 to_fill -= rev_slot.size;
-
                 const moved_file = Slot{
                     .id = rev_slot.id,
                     .type = rev_slot.type,
-                    .start_idx = c_index,
                     .size = rev_slot.size,
                 };
 
+                //check if rev_index + 1 == empty and rev_index - 1 == empty, if so merge
+                // don't do it for the last index
+                if (rev_index.? < (slots.items.len - 1)) {
+                    const rev_slot_minus_1 = slots.items[rev_index.? - 1];
+                    const rev_slot_plus_1 = slots.items[rev_index.? + 1];
+                    if (rev_slot_minus_1.type == 'E' and rev_slot_plus_1.type == 'E') {
+                        const merged_empty = Slot{
+                            .id = rev_slot_minus_1.id,
+                            .type = 'E',
+                            .size = rev_slot_minus_1.size + rev_slot_plus_1.size,
+                        };
+
+                        _ = slots.orderedRemove(rev_index.? + 1);
+                        _ = slots.orderedRemove(rev_index.? - 1);
+                        try slots.insert(rev_index.? - 1, merged_empty);
+                    }
+                }
                 //remove found file in the back
                 _ = slots.orderedRemove(rev_index.?);
 
@@ -220,11 +133,10 @@ pub fn main() !void {
 
                 //if still room left
                 if (to_fill > 0) {
+                    std.debug.print("not filled up, left {}\n", .{to_fill});
                     //insert new empty slot at old_index
-                    const new_empty_idx = c_slot.start_idx + rev_slot.size;
                     const new_empty = Slot{
                         .id = c_slot.id,
-                        .start_idx = new_empty_idx,
                         .type = c_slot.type,
                         .size = to_fill,
                     };
@@ -233,7 +145,11 @@ pub fn main() !void {
 
                 //insert the file in the front
                 try slots.insert(c_index, moved_file);
-                std.debug.print("{any}\n", .{moved_file});
+                print(&slots);
+
+                if (to_fill > 0) {
+                    c_index += 1;
+                }
             }
         }
 
@@ -241,7 +157,14 @@ pub fn main() !void {
     }
 
     //printAll(&empty, &copied, &filled, &files);
-    const result: usize = 0;
+    var result: usize = 0;
+    for (slots.items, 0..) |slot, start_idx| {
+        if (slot.type == 'F') {
+            for (0..(slot.size - 1)) |idx| {
+                result += idx + start_idx * slot.id;
+            }
+        }
+    }
 
     std.debug.print("Result: {}\n", .{result});
 }
