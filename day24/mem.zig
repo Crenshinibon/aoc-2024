b65ef305ffa20a5ed388745e3d3e0f6908c7cce2
Dirk Porsche <d.porsche@verband.creditreform.de>
2025-01-29 16:22:34 +0100

    something

--- day24/mem.zig
+++ day24/mem.zig
@@ -1,4 +1,95 @@
 const std = @import("std");
+const input_r = @embedFile("./input_r.txt");
+const input_s = @embedFile("./input_s.txt");
+
+fn readInputState(allocator: std.mem.Allocator) ![]Node {
+    var state = std.ArrayList(Node).init(allocator);
+
+    var i_ite = std.mem.tokenizeScalar(u8, input_s, '\n');
+    while (i_ite.next()) |i_line| {
+        const id = i_line[0..3];
+        const value = i_line[5];
+
+        try state.append(.{
+            .id = id,
+            .value = if (value - 48 == 0) 0 else 1,
+        });
+    }
+    return state.items;
+}
+
+fn readNodes(allocator: std.mem.Allocator) !std.StringHashMap(*Node) {
+    var map = std.StringHashMap(*Node).init(allocator);
+    var r_ite = std.mem.tokenizeScalar(u8, input_r, '\n');
+
+    while (r_ite.next()) |r_string| {
+        var p_ite = std.mem.tokenizeScalar(u8, r_string, ' ');
+        const in_1 = p_ite.next().?;
+        //discard ops
+        _ = p_ite.next().?;
+        const in_2 = p_ite.next().?;
+        //discard arrow
+        _ = p_ite.next().?;
+        const next = p_ite.next().?;
+
+        if (!map.contains(in_1)) {
+            var from_1 = try allocator.create(Node);
+            from_1.id = in_1;
+            try map.put(in_1, from_1);
+        }
+        if (!map.contains(in_2)) {
+            var from_2 = try allocator.create(Node);
+            from_2.id = in_2;
+            try map.put(in_2, from_2);
+        }
+        if (!map.contains(next)) {
+            var to = try allocator.create(Node);
+            to.id = next;
+            try map.put(next, to);
+        }
+    }
+
+    return map;
+}
+
+fn readTransitions(allocator: std.mem.Allocator, nodes: std.StringHashMap(*Node)) ![]Transition {
+    var trans = std.ArrayList(Transition).init(allocator);
+
+    var r_ite = std.mem.tokenizeScalar(u8, input_r, '\n');
+    while (r_ite.next()) |r_string| {
+        var p_ite = std.mem.tokenizeScalar(u8, r_string, ' ');
+        const in_1 = p_ite.next().?;
+        const ops_string = p_ite.next().?;
+        const in_2 = p_ite.next().?;
+        //discard arrow
+        _ = p_ite.next().?;
+        const next = p_ite.next().?;
+
+        var op: Op = undefined;
+        if (std.mem.eql(u8, ops_string, "OR")) {
+            op = Op.the_or;
+        } else if (std.mem.eql(u8, ops_string, "XOR")) {
+            op = Op.the_xor;
+        } else if (std.mem.eql(u8, ops_string, "AND")) {
+            op = Op.the_and;
+        } else unreachable;
+
+        const from_1 = nodes.get(in_1);
+        const from_2 = nodes.get(in_2);
+        const to = nodes.get(next);
+
+        const r = Transition{
+            .from_1 = from_1.?,
+            .from_2 = from_2.?,
+            .op = op,
+            .to = to.?,
+        };
+
+        try trans.append(r);
+    }
+
+    return trans.items;
+}
 
 const Op = enum {
     the_or,
@@ -6,28 +97,78 @@ const Op = enum {
     the_and,
 
     pub fn doOp(self: Op, in_1: u8, in_2: u8) !u8 {
-        if (self == Op.the_or) {
-            return in_1 | in_2;
-        } else if (self == Op.the_xor) {
-            return in_1 ^ in_2;
-        } else if (self == Op.the_and) {
-            return in_1 & in_2;
-        } else unreachable;
+        return switch (self) {
+            Op.the_or => in_1 | in_2,
+            Op.the_xor => in_1 ^ in_2,
+            Op.the_and => in_1 & in_2,
+        };
+    }
+    pub fn print(self: Op) void {
+        switch (self) {
+            Op.the_or => std.debug.print("OR", .{}),
+            Op.the_xor => std.debug.print("XOR", .{}),
+            Op.the_and => std.debug.print("AND", .{}),
+        }
     }
 };
 
 const Node = struct {
     id: []const u8,
+    value: ?u1 = null,
 };
 
 const Transition = struct {
     from_1: *Node,
     from_2: *Node,
     op: Op,
-    to_1: *Node,
+    to: *Node,
 };
+
+pub fn printNode(n: *Node) void {
+    std.debug.print("{s}({?})", .{ n.id, n.value });
+}
+
+pub fn printTransition(t: *const Transition) void {
+    std.debug.print("Transition: \n", .{});
+    printNode(t.from_1);
+    printNode(t.from_2);
+    std.debug.print("\n", .{});
+    std.debug.print("Op: ", .{});
+    t.op.print();
+    std.debug.print("\n", .{});
+    printNode(t.to);
+    std.debug.print("\n", .{});
+}
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
+
+    const state = try readInputState(allocator);
+    std.debug.print("{any}", .{state});
+
+    const nodes = try readNodes(allocator);
+    const transitions = try readTransitions(allocator, nodes);
+
+    var inTransitions = std.StringHashMap(*const Transition).init(allocator);
+    var targets = std.ArrayList(*const Transition).init(allocator);
+    for (transitions) |t| {
+        try inTransitions.put(t.to.id, &t);
+        if (t.to.id[0] == 'z') {
+            try targets.append(&t);
+        }
+        std.debug.print("Transitions Loop: ", .{});
+        printTransition(&t);
+    }
+
+    var valIte = inTransitions.valueIterator();
+    while (valIte.next()) |t| {
+        std.debug.print("TMap Loop: ", .{});
+        printTransition(t.*);
+    }
+
+    for (targets.items) |t| {
+        std.debug.print("Targets loop: ", .{});
+        printTransition(t);
+    }
 }
--- day24/parttwo.ts
+++ day24/parttwo.ts
@@ -2,31 +2,107 @@ const xor = (v1: number, v2: number): number => v1 ^ v2;
 const or = (v1: number, v2: number): number => v1 | v2;
 const and = (v1: number, v2: number): number => v1 & v2;
 
-interface Node {
+interface NNode {
   id: string;
+  value?: number;
+  incoming?: Transition;
+  inputsFor: (value: number) => NNode;
 }
 
 interface Transition {
-  from_1: Node;
-  from_2: Node;
+  from_1: NNode;
+  from_2: NNode;
   op: (v1: number, v2: number) => number;
-  to: Node;
+  to: NNode;
 }
 
-const rFile = Bun.readTextFile("./small_r.txt");
-for (const l of rFile.split("\n")) {
-  const parts = l.split("\s");
+const nodes: Record<string, NNode> = {};
+const transitions: Transition[] = [];
 
+const file = Bun.file("./input_r.txt");
+const text = await file.text();
+
+for (const l of text.split("\n")) {
+  if (l.length == 0) continue;
+
+  const parts = l.split(/\s/);
   const in1 = parts[0];
   const opString = parts[1];
   const in2 = parts[2];
   const out = parts[4];
 
-  let nodes: Record<string, Node> = {};
-  let transitions: Transition[] = [];
-
+  let op: null | ((v1: number, v2: number) => number);
   if (opString == "XOR") {
+    op = xor;
   } else if (opString == "OR") {
+    op = or;
   } else if (opString == "AND") {
+    op = and;
+  } else {
+    console.log(opString);
+    throw Error(opString);
   }
+
+  let in1Node: NNode = nodes[in1];
+  if (!in1Node) {
+    in1Node = {
+      id: in1,
+    };
+    nodes[in1] = in1Node;
+  }
+
+  let in2Node: NNode = nodes[in2];
+  if (!in2Node) {
+    in2Node = {
+      id: in2,
+    };
+    nodes[in2] = in2Node;
+  }
+
+  let outNode: NNode = nodes[out];
+  if (!outNode) {
+    outNode = {
+      id: out,
+    };
+  }
+
+  const t: Transition = {
+    from_1: in1Node,
+    from_2: in2Node,
+    op: op,
+    to: outNode,
+  };
+
+  outNode.incoming = t;
+  nodes[out] = outNode;
+
+  transitions.push(t);
 }
+
+const targetNodes = Object.values(nodes).filter((n) => n.id[0] == "z");
+
+const traverse = (tNode: NNode, collector: Record<string, NNode>) => {
+  collector[tNode.id] = tNode;
+  if (tNode.incoming) {
+    //console.log("Following:", tNode.incoming.from_1.id);
+    traverse(tNode.incoming.from_1, collector);
+    //console.log("Following:", tNode.incoming.from_2.id);
+    traverse(tNode.incoming.from_2, collector);
+  } else {
+    //console.log("DONE");
+  }
+};
+
+const dependingNodes: Record<string, NNode> = {};
+traverse(targetNodes[0], dependingNodes);
+
+targetNodes[0].inputsFor(1);
+
+console.log(
+  Object.values(dependingNodes)
+    .filter((d) => d.id[0] == "x" || d.id[0] == "y")
+    .sort((d1, d2) => {
+      return parseInt(d1.id.substring(1)) - parseInt(d2.id.substring(1));
+    })
+    .map((d) => d.id),
+);
